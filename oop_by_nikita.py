bc7275b27db66dc655fde6f5ba21025371172bda
Isaac Munyaka <isaacmunyaka98@gmail.com>
2024-04-16 21:59:59 +0300

    Added files

---
+++ oop_by_nikita.py
@@ -0,0 +1,27 @@
+class Student:
+    pass
+
+
+    def exam(self):# method under the student class
+        print("Student is eligible to sit for exam")
+
+    def admission(self):
+        print("Welcome to Our School")
+
+        
+
+obj= Student() #instance/object
+obj2 =Student()
+
+
+# class Finance:
+    # pass
+
+# obj2= Finance()
+print(obj)
+print(obj2)
+
+obj.exam()
+obj2.exam()
+obj.admission()
+obj2.admission()
